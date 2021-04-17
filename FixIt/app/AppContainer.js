682ea0a033a7ae8324c6737c666d547edafa02e4
anhndse05746 <anhnd128@gmail.com>
2021-04-18 05:05:27 +0700

    add message handler

--- FixIt/app/AppContainer.js
+++ FixIt/app/AppContainer.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {createStackNavigator} from '@react-navigation/stack';
-import {NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 import Navigation from './libs/Navigation';
-import {getActiveRouteName} from './utils/navigation';
-import {connect} from 'react-redux';
+import { getActiveRouteName } from './utils/navigation';
+import { connect } from 'react-redux';
 import {
   ROOT_BACKGROUND,
   ROOT_INSIDE,
@@ -14,14 +14,40 @@ import {
 import AuthLoadingView from './screens/views/AuthLoadingView';
 import DrawerInside from './screens/stacks/DrawerStack';
 import OutsideStackNavigator from './screens/stacks/OutsideStack';
-import {StatusBar} from 'react-native';
+import { StatusBar } from 'react-native';
+
+import { Alert } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 const Stack = createStackNavigator();
-const App = React.memo(({root}) => {
+const App = React.memo(({ root }) => {
+
+  messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Message handled in the background!', remoteMessage);
+    //navigation.navigate(remoteMessage.data.screen, { requestData: { id: remoteMessage.data.requestId } });
+
+  });
+
+  messaging().onNotificationOpenedApp(remoteMessage => {
+    console.log(
+      'Notification caused app to open from background state:',
+      remoteMessage.notification,
+    );
+    navigation.navigate(remoteMessage.data.screen);
+  });
+
   React.useEffect(() => {
     const state = Navigation.navigationRef.current?.getRootState();
     const currentRouteName = getActiveRouteName(state);
     Navigation.routeNameRef.current = currentRouteName;
+  }, []);
+
+  React.useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
   }, []);
 
   return (
@@ -36,7 +62,7 @@ const App = React.memo(({root}) => {
         Navigation.routeNameRef.current = currentRouteName;
       }}>
       <StatusBar hidden />
-      <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <>
           {root === ROOT_LOADING || root === ROOT_BACKGROUND ? (
             <Stack.Screen name="AuthLoading" component={AuthLoadingView} />
